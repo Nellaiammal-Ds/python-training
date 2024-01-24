04d14f4f902b1908b2bf1640c39c80e11308b037
Nellaiammal-Ds <nellaiammal.s@datasirpi.com>
2024-01-24 15:15:03 +0530

    loop

---
+++ loop.py
@@ -0,0 +1,34 @@
+     # while loop
+i=1
+while i < 6:
+    print(i)
+    i+=1
+
+    # break statement
+
+
+a =1
+while a < 6:
+    a += 1
+    if (a == 3):
+        break
+        print(a)
+# continue
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
+
+  # for loop
+
+fruits = ("banana","mango","cherry")
+for x in fruits:
+    print(x)
+ # with break statement
+fruit= ("banana","mango","cherry")
+for x in fruit:
+ if x == "mango":
+    break
+    print(x)
