0d6293bdca2e7fac197ed4e635aec3adea9a1c61
Nellaiammal-Ds <nellaiammal.s@datasirpi.com>
2024-01-24 15:57:57 +0530

    multipleconditions

---
+++ multipleconditions.py
@@ -0,0 +1,16 @@
+
+# conditions like ==, <, >,
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+
+a = 34
+b = 16
+if b < a:
+ print("b is less than a")
+
+a = 29
+b = 29
+if b == a:
+ print("b is equal to a")
