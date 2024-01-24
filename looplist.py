a72b62dd03b324d4cd2644430a45e34bd0648501
Nellaiammal-Ds <nellaiammal.s@datasirpi.com>
2024-01-24 17:15:48 +0530

    looplist done

---
+++ looplist.py
@@ -0,0 +1,18 @@
+
+             #loop - lists
+list = ["banana","cherry","orange"]
+for x in list:
+    print(x)
+
+# loop through the index  number
+list1 = ["banana","cherry","orange"]
+for i in range(len(list1)):
+    print(list1[i])
+
+    # using a while loop in a list
+
+list2 = ["apple","banana","orange"]
+  i = 0
+ while i < len(list2):
+     print(list2[i])
+     i = i + 1
