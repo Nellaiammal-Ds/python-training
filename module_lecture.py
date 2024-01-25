8274244895a193d0916a2e11077cf2ea610b89ae
Nellaiammal-Ds <nellaiammal.s@datasirpi.com>
2024-01-25 13:02:43 +0530

    modules done

---
+++ module_lecture.py
@@ -0,0 +1,12 @@
+# import all dunctions usimg *
+from my_module import *
+
+print(area_of_square(4))
+calculator(3,2)
+
+# importing my_module
+
+import my_module
+calculator(3,2)
+print(area_of_square(5))
+print(my_module.a)
