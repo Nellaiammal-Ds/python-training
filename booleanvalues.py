8e647cb0d04667210fd8d1eff854fe24b4edae09
Nellaiammal-Ds <nellaiammal.s@datasirpi.com>
2024-01-25 11:15:00 +0530

    Booleanvalues done

---
+++ booleanvalues.py
@@ -0,0 +1,24 @@
+# boolean value true or false
+
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+# print a message based on true or false
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+# evalute values using bool() function
+print(bool("hello"))
+print(bool(123))
+
+# evalute variables using bool() function
+x = "hello"
+y = "15"
+
+print(bool(x))
+print(bool(y))
