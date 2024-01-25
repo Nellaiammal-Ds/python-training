d8832184e4b1ba6d8909af619424655ee1678fb1
Nellaiammal-Ds <nellaiammal.s@datasirpi.com>
2024-01-25 11:04:13 +0530

    Inheritance done

---
+++ Inheritance.py
@@ -0,0 +1,36 @@
+
+# create a parent class
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+#Use the Person class to create an object, and then execute the printname method:
+
+x = Person("Antony", "Doe")
+x.printname()
+# create child class
+class Student(Person):
+  pass
+x = Student("Anu", "antony")
+x.printname()
+
+     # using super()
+
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+class Student(Person):
+  def __init__(self, fname, lname):
+    super().__init__(fname, lname)
+
+x = Student("Antony", "louis")
+x.printname()
