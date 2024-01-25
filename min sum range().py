f73065c1e28b806e7b8c58dd50c9a3cf5fe1c885
Nellaiammal-Ds <nellaiammal.s@datasirpi.com>
2024-01-25 12:06:42 +0530

    minsumrange done

---
+++ min sum range().py
@@ -0,0 +1,28 @@
+# min()
+x = min(5, 10)
+
+print(x)
+
+#sum()
+a = (1, 2, 3, 4, 5)
+x = sum(a)
+print(x)
+
+# range()
+x = range(6)
+for n in x:
+  print(n)
+
+  # range () using start and stop
+
+x = range(6,10)
+for n in x:
+ print(n)
+
+# range() using start, stop,step
+x = range(6,25,4)
+for n in x:
+  print(n)
+
+
+
