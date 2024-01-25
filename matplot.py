b874041932e2d0cfe3827f70c95632d7f2a95bad
Nellaiammal-Ds <nellaiammal.s@datasirpi.com>
2024-01-25 10:01:29 +0530

    matplot done

---
+++ matplot.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','saturday']
+numbers = [24,25,18,12,20,19,11]
+plt.plot(days,numbers)
+plt.xlabel('Days')
+plt.ylabel('number of push-ups')
+plt.title('My push-ups graph')
+plt.show()
+
+
