6dae3d09cce03033fcc61d6b8eb6f922c83ffcd3
Nellaiammal-Ds <nellaiammal.s@datasirpi.com>
2024-01-25 10:19:03 +0530

    Passstatement done

---
+++ pass.py
@@ -0,0 +1,14 @@
+#pass in loops
+for i in range(10):
+    if i==5:
+        pass
+        print("pass statement")
+    print(i)
+
+#pass in function
+def myfun():
+    pass
+
+#pass in class
+class demo:
+    pass
