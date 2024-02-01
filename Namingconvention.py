65b086a6242e17b10c8b211199364e5829bf12d4
Nellaiammal-Ds <nellaiammal.s@datasirpi.com>
2024-02-01 13:03:26 +0530

    naming convention is done

---
+++ Namingconvention.py
@@ -0,0 +1,25 @@
+# function name should be lowercase with underscore
+def calculate_square_area(side_length):
+    return side_length ** 2
+
+# variable name should be lowercase
+radius = 5
+
+# class name should be uppercase
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+# function name should be lowercase with underscore
+    def calculate_circumference(self):
+        return 2 * 3.14 * self.radius
+
+if __name__ == "__main__":
+    
+
+    square_area = calculate_square_area(4)
+    print("Square Area:", square_area)
+
+    # objectname should be lowercase
+    circle_obj = Circle(radius)
+    circumference = circle_obj.calculate_circumference()
+    print("Circle Circumference:", circumference)
